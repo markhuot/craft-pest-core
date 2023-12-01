8fc77b94b3edc9769fb5cfe789baad04a95aed61
markhuot <mark@markhuot.com>
2023-12-01 14:00:52 -0500

    non-interactive on subprocess

--- src/test/TestCase.php
+++ src/test/TestCase.php
@@ -152,7 +152,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
     protected function craftProjectConfigApply()
     {
         $craftExePath = getenv('CRAFT_EXE_PATH') ?: './craft';
-        $process = new Process([$craftExePath, 'project-config/apply', '--force']);
+        $process = new Process([$craftExePath, 'project-config/apply', '--force', '--interactive=0']);
         $process->setTty(Process::isTtySupported());
         $process->setTimeout(null);
         $process->start();
