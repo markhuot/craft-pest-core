9c91a16aea4ff930500ed6638e8e6af7fb0e512d
markhuot <mark@markhuot.com>
2024-03-20 16:19:16 -0400

    fixing phpstan version constraints

--- phpstan.php
+++ phpstan.php
@@ -5,11 +5,11 @@ use Composer\Semver\VersionParser;
 
 $config = [];
 
-if (InstalledVersions::satisfies(new VersionParser, 'craftcms/cms', '~4.0.0')) {
+if (InstalledVersions::satisfies(new VersionParser, 'craftcms/cms', '~4.0')) {
     $config['includes'][] = 'phpstan-craft4.neon';
 }
 
-if (InstalledVersions::satisfies(new VersionParser, 'craftcms/cms', '~5.0.0')) {
+if (InstalledVersions::satisfies(new VersionParser, 'craftcms/cms', '~5.0')) {
     $config['includes'][] = 'phpstan-craft5.neon';
 }
 
