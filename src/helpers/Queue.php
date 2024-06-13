e31cfb4dc24e934dfc25b196c96a7bfdbf27d5fe
markhuot <mark@markhuot.com>
2024-06-13 14:03:08 -0400

    add queue helper

---
+++ src/helpers/Queue.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace markhuot\craftpest\helpers\queue;
+
+use Craft;
+
+if (! function_exists('queue')) {
+    function queue($callback) {
+        $result = $callback();
+
+        Craft::$app->queue->run();
+
+        return $result;
+    }
+}
