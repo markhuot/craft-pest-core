f5f248fb1602da794b7d46b525db8ad3533d97d0
markhuot <mark@markhuot.com>
2025-03-25 10:34:49 -0400

    debugging

--- src/test/TestCase.php
+++ src/test/TestCase.php
@@ -74,13 +74,14 @@ class TestCase extends \PHPUnit\Framework\TestCase
             $needsRefresh = true;
         }
 
-//        if (
-//            Craft::$app->getMigrator()->getNewMigrations() ||
-//            Craft::$app->getContentMigrator()->getNewMigrations()
-//        ) {
-//            $this->craftMigrateAll();
-//            $needsRefresh = true;
-//        }
+        if (
+            Craft::$app->getMigrator()->getNewMigrations() ||
+            Craft::$app->getContentMigrator()->getNewMigrations()
+        ) {
+            \markhuot\craftpest\helpers\test\dd('has migrations');
+            $this->craftMigrateAll();
+            $needsRefresh = true;
+        }
 
         // We have to flush the data cache to make sure we're getting an accurate look at whether or not there
         // are pending changes.
