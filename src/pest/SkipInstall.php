a70921d55d07d592c091d5956c95cbb450ee5f55
markhuot <mark@markhuot.com>
2025-12-05 17:19:11 -0500

    refactor SkipInstall to use a direct string for the skip-install option

--- src/pest/SkipInstall.php
+++ src/pest/SkipInstall.php
@@ -6,20 +6,16 @@ use Pest\Contracts\Plugins\HandlesArguments;
 
 class SkipInstall implements HandlesArguments
 {
-    private const SKIP_INSTALL_OPTION = 'skip-install';
-
     public static bool $skipInstall = false;
 
     public function handleArguments(array $originals): array
     {
-        $option = '--'.self::SKIP_INSTALL_OPTION;
-
-        if (! in_array($option, $originals)) {
+        if (! in_array('--skip-install', $originals)) {
             return $originals;
         }
 
         self::$skipInstall = true;
 
-        return array_values(array_filter($originals, fn ($arg) => $arg !== $option));
+        return array_values(array_filter($originals, fn ($arg) => $arg !== '--skip-install'));
     }
 }
