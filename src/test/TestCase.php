27c74bb5ab21dd6d4357d1b92d71bc969186c7e1
markhuot <mark@markhuot.com>
2025-12-05 17:43:30 -0500

    replace SkipInstall with InstallsCraft for improved installation handling

--- src/test/TestCase.php
+++ src/test/TestCase.php
@@ -3,16 +3,8 @@
 namespace markhuot\craftpest\test;
 
 use Craft;
-use craft\enums\CmsEdition;
-use craft\helpers\App;
-use craft\migrations\Install;
-use craft\models\Site;
-use craft\services\ProjectConfig;
 use Illuminate\Support\Collection;
 use markhuot\craftpest\actions\CallSeeders;
-use markhuot\craftpest\http\TestController;
-use markhuot\craftpest\interfaces\RenderCompiledClassesInterface;
-use markhuot\craftpest\pest\SkipInstall;
 use markhuot\craftpest\web\TestableResponse;
 
 class TestCase extends \PHPUnit\Framework\TestCase
@@ -36,12 +28,7 @@ class TestCase extends \PHPUnit\Framework\TestCase
 
     protected function setUp(): void
     {
-        $this->createApplication();
-
         $this->callTraits('setUp');
-
-        // Have to do this after setup to make sure the app is installed first
-        $this->renderCompiledClasses();
     }
 
     protected function tearDown(): void
@@ -65,160 +52,6 @@ class TestCase extends \PHPUnit\Framework\TestCase
                 $this->{$method}();
             }
         }
-    }
-
-    protected function logStart(string $message): float
-    {
-        try {
-            $output = \Pest\Support\Container::getInstance()->get(\Symfony\Component\Console\Output\OutputInterface::class);
-            $output->writeln("  <fg=gray>{$message}</>");
-        } catch (\Throwable) {
-            fwrite(STDOUT, $message . "\n");
-        }
-        return microtime(true);
-    }
-
-    protected function logEnd(string $message, float $start): void
-    {
-        $duration = round(microtime(true) - $start, 2);
-        try {
-            $output = \Pest\Support\Container::getInstance()->get(\Symfony\Component\Console\Output\OutputInterface::class);
-            $output->writeln("  <fg=green>{$message}</> <fg=gray>({$duration}s)</>");
-        } catch (\Throwable) {
-            fwrite(STDOUT, "{$message} ({$duration}s).\n");
-        }
-    }
-
-    public function createApplication()
-    {
-        if (! $this->needsRequireStatements()) {
-            return Craft::$app;
-        }
-
-        $this->requireCraft();
-
-        if (SkipInstall::$shouldSkip) {
-            return Craft::$app;
-        }
-
-        if (! Craft::$app->getIsInstalled(true)) {
-            $start = $this->logStart('Installing Craft CMS...');
-            $this->craftInstall();
-            $this->logEnd('Craft CMS installed', $start);
-        }
-
-        if (
-            Craft::$app->getContentMigrator()->getNewMigrations()
-        ) {
-            $start = $this->logStart('Running migrations...');
-            $this->craftMigrateAll();
-            $this->logEnd('Migrations complete', $start);
-        }
-
-        // We have to flush the data cache to make sure we're getting an accurate look at whether or not there
-        // are pending changes.
-        //
-        // If you are using a separate test database from your dev database you may have an updated project
-        // config on the dev side and have cached that the project-config is updated. Then, when you run the
-        // tests you'll reach in to the same cache as the dev side and pull that the project config is unchanged
-        // even though it actually _is_ changed. This ensures that there isn't any cache sharing between dev
-        // and test.
-        Craft::$app->getCache()->flush();
-        if (Craft::$app->getProjectConfig()->areChangesPending(null, true)) {
-            $start = $this->logStart('Applying project config changes...');
-            $this->craftProjectConfigApply();
-            $this->logEnd('Project config applied', $start);
-        }
-
-        return Craft::$app;
-    }
-
-    protected function craftInstall()
-    {
-        $args = [
-            'username' => (App::env('CRAFT_INSTALL_USERNAME') ?? 'user@example.com'),
-            'email' => (App::env('CRAFT_INSTALL_EMAIL') ?? 'user@example.com'),
-            'password' => (App::env('CRAFT_INSTALL_PASSWORD') ?? 'secret'),
-            'siteName' => (App::env('CRAFT_INSTALL_SITENAME') ?? '"Craft CMS"'),
-            'siteUrl' => (App::env('CRAFT_INSTALL_SITEURL') ?? 'http://localhost:8080'),
-            'language' => (App::env('CRAFT_INSTALL_LANGUAGE') ?? 'en-US'),
-        ];
-
-        $siteConfig = [
-            'name' => $args['siteName'],
-            'handle' => 'default',
-            'hasUrls' => true,
-            'baseUrl' => $args['siteUrl'],
-            'language' => $args['language'],
-            'primary' => true,
-        ];
-
-        $site = new Site($siteConfig);
-
-        $migration = new Install([
-            'db' => \Craft::$app->getDb(),
-            'username' => $args['username'],
-            'password' => $args['password'],
-            'email' => $args['email'],
-            'site' => $site,
-        ]);
-
-        $migrator = Craft::$app->getMigrator();
-        $migrator->migrateUp($migration);
-
-        Craft::$app->getProjectConfig()->reset();
-        Craft::$app->getProjectConfig()->applyExternalChanges();
-        Craft::$app->getProjectConfig()->saveModifiedConfigData();
-
-        $edition = Craft::$app->getProjectConfig()->get('system.edition');
-        if (method_exists(App::class, 'editionIdByHandle')) {
-            Craft::$app->setEdition(App::editionIdByHandle($edition));
-        } elseif (class_exists(CmsEdition::class)) {
-            Craft::$app->setEdition(CmsEdition::fromHandle($edition));
-        } else {
-            throw new \RuntimeException('Could not determine a [system.edition] based on the project config.');
-        }
-    }
-
-    protected function craftMigrateAll()
-    {
-        Craft::$app->getContentMigrator()->up();
-    }
-
-    protected function craftProjectConfigApply()
-    {
-        // applyExternalChanges is going to add event listeners automatically (because internally it makes calls to
-        // ->reset() which calls ->int() which adds event listeners). Normally, this is fine because it gets called
-        // at the _end_ of a request lifecycle. But in our case we're calling it early in the lifecycle and that's
-        // adding duplicate listeners. So we'll remove the listeners here so they can be re-added without duplication.
-        $projectConfig = Craft::$app->getProjectConfig();
-        $projectConfig->off(ProjectConfig::EVENT_ADD_ITEM, [$projectConfig, 'handleChangeEvent']);
-        $projectConfig->off(ProjectConfig::EVENT_UPDATE_ITEM, [$projectConfig, 'handleChangeEvent']);
-        $projectConfig->off(ProjectConfig::EVENT_REMOVE_ITEM, [$projectConfig, 'handleChangeEvent']);
-
-        Craft::$app->getProjectConfig()->applyExternalChanges();
-    }
-
-    public function renderCompiledClasses()
-    {
-        if (! Craft::$app->isInstalled) {
-            return;
-        }
-
-        Craft::$container->get(RenderCompiledClassesInterface::class)->handle();
-    }
-
-    protected function needsRequireStatements()
-    {
-        return ! defined('CRAFT_BASE_PATH');
-    }
-
-    protected function requireCraft()
-    {
-        require __DIR__.'/../bootstrap/bootstrap.php';
-
-        // Set a bogus controller so plugins can interact with Craft:$app->controller without erroring
-        Craft::$app->controller = new TestController('test-controller', Craft::$app);
     }
 
     /**
