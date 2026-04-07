22de0149a13c6eaa32e898f5a842c601a8cd5d9b
markhuot <mark@markhuot.com>
2026-04-07 09:43:15 -0400

    fix: resolve short class names in @see tags via use statements
    The doc generator now parses use statements from source files to
    resolve short class names in @see tags before reflecting them.

--- src/factories/Asset.php
+++ src/factories/Asset.php
@@ -45,7 +45,7 @@ class Asset extends Element
      *
      * Commonly, you will want to set this to an a temporary volume, that is only used in tests.
      *
-     * @see markhuot\craftpest\test\LocalVolumes
+     * @see LocalVolumes
      */
     public function volume(string $handle)
     {
